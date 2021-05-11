533ed128bb63fc2a244a0e22017a337802f49fcb
Jyoti Gaikwad <jyotiigaikwad01@gmail.com>
2021-05-11 22:07:10 +0530

    Excel Data

---
+++ excelData.java
@@ -0,0 +1,33 @@
+package WorksjoyExcelData;
+
+import ExcelTest.com.excel.Xls_Reader;
+
+import java.util.ArrayList;
+
+public class excelData {
+    static Xls_Reader reader;
+
+    public static ArrayList<Object[]> getDataFromExcel() {
+
+        ArrayList<Object[]> myExcelData = new ArrayList<Object[]>();
+
+        reader = new Xls_Reader("C:/Users/Admin.DESKTOP-6CD7RG7/IdeaProjects/Worksjoy Project/src/LoginFunctionality/WorksjoyLoginData.xlsx");
+        int rowCount = reader.getRowCount("LoginData");
+
+        for (int i = 2; i < rowCount; i++) {
+            String username = reader.getCellData("LoginData", "username", i);
+
+            String password = reader.getCellData("LoginData", "password", i);
+
+            Object [] obj = {username,password};
+            myExcelData.add(obj);
+
+
+        }
+
+        return myExcelData;
+
+
+    }
+
+}
